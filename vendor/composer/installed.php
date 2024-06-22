50605f212c66eff8bb9fe197acd34e082f4f0f6c
Ankith G <ankithg03@gmail.com>
2024-06-22 23:42:23 +0530

    Added a first php file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendorname/packagename',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c4bf124d562695fa299b5b7f578466267446c63',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendorname/packagename' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c4bf124d562695fa299b5b7f578466267446c63',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
